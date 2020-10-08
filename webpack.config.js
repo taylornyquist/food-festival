b4e3cd850776602d548e06f3dc6aac8c518fd8a5
Taylor Nyquist <tnyquist@gmail.com>
2020-10-08 18:51:57 -0500

    split JS files into modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports =
 {
@@ -13,6 +14,9 @@ module.exports =
             $: "jquery",
             jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+        })
     ],
     mode: 'development'
 };
