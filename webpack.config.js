88c43befa0987ab490c2ad814ecb7da31a361fa6
Taylor Nyquist <tnyquist@gmail.com>
2020-10-09 09:45:26 -0500

    created pwa manifest

--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,20 @@ const config = {
         }),
         new BundleAnalyzerPlugin({
             analyzerMode: "disable", // "static" the report outputs to an HTML file in the dist folder, or set to "disable"
+        }),
+        new WebpackPwaManifest({
+            name: "Food Event",
+            short_name: "Foodies",
+            description: "An app that allows you to view upcoming food events.",
+            background_color: "#01579b",
+            theme_color: "#ffffff",
+            fingerprints: false,
+            inject: false,
+            icons: [{
+                src: path.resolve("assets/img/icons/icon-512x512.png"),
+                sizes: [96, 128, 192, 256, 384, 512],
+                destination: path.join("assets", "icons")
+            }]
         })
     ],
     mode: "development"
